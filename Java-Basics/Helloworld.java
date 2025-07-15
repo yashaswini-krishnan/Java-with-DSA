e61c996fc893a455c80de2595d08cf5fb197dee5
yashaswini-krishnan <yashaswini.kriishnan@gmail.com>
2025-07-15 14:34:10 +0530

    Helloworld.java

--- Java-Basics/Helloworld.java
+++ Java-Basics/Helloworld.java
@@ -1,8 +1,37 @@
-package basics;
+
+/*three important concepts before starting
+ * 1) Methods/functions
+ * small block of code that executes a specific task
+ * can be called anywhere in the class
+ * often ends with paranthesis ()
+ * ex: main(), print(), loginUser()
+ * 
+ * 2)Class:
+ * Blueprint used for creating object 
+ * contain attributes(variables): string username; and methods(functions):register(){..}
+ * all the java code is written inside the class
+ * 
+ * 3)Packages:
+ * Think of them as folders that organize Java classes
+ * ex: java.io.* contains pre-written code for I/O operations
+ */
+
+/* Keywords:
+ * public: Makes the method or class accessible from anywhere
+ * static: Belongs to the class, not instances (can run without creating an object)
+ * void: method returns nothing
+ * main(): The entry point method Java looks for during execution
+ * String[]:Array of strings to accept input from the command line
+ * args: Variable that stores the command line input values
+ */
+
+package basics; //package name(acts like a folder)
 
 public class Helloworld {
 		public static void main(String[] args) {
-			System.out.println("Hello java");
+			//syntax: rules for writing the code in java
+			//statement: A single instruction (ends with ;)
+			System.out.println("Hello java"); //output line
 		}
 
 	}
